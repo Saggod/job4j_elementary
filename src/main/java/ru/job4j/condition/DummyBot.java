97c96aa2f05a1bcbece7aa2df76e0789813fe832
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-09-28 12:26:34 +0300

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -3,13 +3,11 @@ package ru.job4j.condition;
 public class DummyBot {
 
     public static String answer(String question) {
-        String rs1;
+        String rs1 = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rs1 = "Привет, умник!";
         } else if ("Пока.".equals(question)) {
             rs1 = "До скорой встречи.";
-        } else {
-            rs1 = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
         return rs1;
     }
