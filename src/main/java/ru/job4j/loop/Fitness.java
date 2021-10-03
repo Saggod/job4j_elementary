9344c1496a6ef08e676b54536ae2ab838e24f275
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-10-03 14:12:11 +0300

    5.2. Создать программу, вычисляющую факториал. [#193 #211356]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Fitness {
+
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+           nik = nik / 2;
+           ivan = ivan / 3;
+           return  (nik - ivan) / 3;
+        }
+        return month;
+    }
+
+}
