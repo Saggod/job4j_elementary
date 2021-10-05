cadba29f9a3b7a65ceb9d1cf18abd3d2702b2075
Aleksei.Zhabura <kkthxkk@gmail.com>
2021-10-05 14:03:21 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames = " + surnames.length);
         System.out.println("Размер массива prices = " + prices.length);
 
+        String[] names = new String[4];
+        names[0] = "Aleksei Zhabura";
+        names[1] = "Vasiliy Pupkin";
+        names[2] = "Noname Nosuname";
+        names[3] = "Ivan Sokolov";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
